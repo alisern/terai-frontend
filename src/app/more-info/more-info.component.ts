f5a634604d057955c7500a2b92d87ab21151b9bd
ievabk <ltieva5@gmail.com>
2020-03-18 17:58:46 +0200

    more info with collapses

--- src/app/more-info/more-info.component.ts
+++ src/app/more-info/more-info.component.ts
@@ -10,6 +10,12 @@ export class MoreInfoComponent implements OnInit {
 
   constructor() { }
   isCollapsed = true;
+  isCollapsed1 = true;
+  isCollapsed2 = true;
+  isCollapsed3 = true;
+  isCollapsed4 = true;
+  isCollapsed5 = true;
+  isCollapsed6 = true;
   ngOnInit(): void {
     
   }
