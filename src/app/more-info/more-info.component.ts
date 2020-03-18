a2a8dbdda484d02c23351fff06481a15e0bb7c85
ievabk <ltieva5@gmail.com>
2020-03-18 15:53:43 +0200

    questions added

--- src/app/more-info/more-info.component.ts
+++ src/app/more-info/more-info.component.ts
@@ -9,11 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class MoreInfoComponent implements OnInit {
 
   constructor() { }
-
+  isCollapsed = true;
   ngOnInit(): void {
   }
 
 }
-export class CollapseDemoComponent {
-  isCollapsed = false;
-}
+
+
